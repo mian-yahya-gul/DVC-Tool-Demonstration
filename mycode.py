fce07eaa05602cd4cdef03926e3963aae910b586
mian-yahya-gul <kakakhel176@gmail.com>
2025-09-02 12:04:44 +0500

    second version of data stored

--- mycode.py
+++ mycode.py
@@ -11,8 +11,8 @@ data = { 'Name': ['Alice','Bob', 'Charlie'],
 df = pd.DataFrame(data)
 
 # Adding new row for v2
-# new_row_loc = { 'Name' : 'GF1', 'Age' : 20, 'City' : 'Chicago'}
-# df.loc[len(df.index)] = new_row_loc
+new_row_loc = { 'Name' : 'GF1', 'Age' : 20, 'City' : 'Chicago'}
+df.loc[len(df.index)] = new_row_loc
 
 #adding new row for v3
 # new_row_loc2 = {'Name': 'GF2', 'Age': 25, "City": 'Peshawar'}
